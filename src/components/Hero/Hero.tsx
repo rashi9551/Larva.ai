57f76277e77316a376fabe89365f0e39b58a5013
rashi9551 <rashidroz1361@gmail.com>
2025-06-18 18:26:42 +0530

    done3

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,7 +6,6 @@ export const Hero = () => {
       <video
         autoPlay
         muted
-        playsInline
         loop
         className="fixed top-0 left-0 w-full h-full object-cover -z-30 rotate-180 transform -translate-y-[52.5vh]"
       >
